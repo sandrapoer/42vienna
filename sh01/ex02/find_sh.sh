964ac9bdb036f498ead155f37e44d0d9d950d174
Sandra Poernbacher <spoernba@c2r4p8.42vienna.com>
2026-08-17 12:18:06 +0200

    adjust exec command

--- sh01/ex02/find_sh.sh
+++ sh01/ex02/find_sh.sh
@@ -1,1 +1,8 @@
-find . -type f -exec basename -s .sh {} \;
+find . -name "*.sh" -exec basename -s .sh {} \;
+
+# find .               -> recursively 
+# -type f              -> files
+# -name "*.sh"         -> ending in .sh
+# exec basename -s .sh -> extract name of file (basename) without path and remove suffix 
+# {}                   -> placeholder for paht that find found
+# \;                   -> end of execution command
