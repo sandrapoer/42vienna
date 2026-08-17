6a29c7175b1e03b302828263ed2424be4e2fffe7
sandrapoer <sandrapoernbacher@gmail.com>
2026-08-17 15:34:34 +0200

    corrected version

--- sh01/ex07/r_dwssap.sh
+++ sh01/ex07/r_dwssap.sh
@@ -1,1 +1,10 @@
 cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | cut -d: -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | paste -sd, - | sed 's/,/, /g; s/$/./'
+
+# show content of system login
+# remove lines with comments = starting with #
+# -n suppresses automatic printin, n skips line, p prints line
+# ectracts column before : -> username
+# reverse character order
+# print lines of given variable range including line2 value
+# join with commas
+# comma separated and last ends with .
