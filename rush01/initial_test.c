3a1ff640a4b5dffec5877f2eb7255179e376f4ca
sandrapoer <sandrapoernbacher@gmail.com>
2026-08-22 14:47:30 +0200

    grid for fixed size 4x4

--- rush01/initial_test.c
+++ rush01/initial_test.c
@@ -4,57 +4,36 @@
 // Grid size = nxn
 // input size = n² (array len including spaces = nx2-1)
 
-int *get_grid(char *input, int *out_size)
+
+#define SIZE 4
+
+static void print_grid(int grid[SIZE][SIZE])
 {
-    int i;
-    int len;
-    int size;
-    int *grid;
+    char c;
+    int  row;
+    int  col;
 
-    i = 0;
-    while (input[i])
-        i++;
-    len = i;
-    size = (len + 1) / 2;
-
-    grid = malloc(sizeof(int) * size * size);
-    if (!grid)
-        return (NULL);
-
-    i = 0;
-    while (i < size * size)
+    row = 0;
+    while (row < SIZE)
     {
-        grid[i] = 0;
-        i++;
+        col = 0;
+        while (col < SIZE)
+        {
+            c = (char)('0' + grid[row][col]);
+            write(1, &c, 1);
+            if (col < SIZE - 1)
+                write(1, " ", 1);
+            col++;
+        }
+        write(1, "\n", 1);
+        row++;
     }
-
-    *out_size = size;
-    return (grid);
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-    int size;
-    int *grid;
-    int i;
-    char ch;
+    int grid[SIZE][SIZE] = {0};
 
-    if (argc != 2)
-        return (1);
-    grid = get_grid(argv[1], &size);
-    if (!grid)
-        return (1);
-    i = 0;
-    while (i < size * size)
-    {
-        ch = (char)('0' + grid[i]);
-        write(1, &ch, 1);
-        if ((i + 1) % size == 0)
-            write(1, "\n", 1);
-        else
-            write(1, " ", 1);
-        i++;
-    }
-    free(grid);
+    print_grid(grid);
     return (0);
 }
